90d460514fbe0d6596ca3bd086d8e9127eb6fa5a
Michael Simmonds <michaelsimmonds92@hotmail.com>
2018-10-15 00:10:30 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,21 @@ console.log("Up and running!");
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log("User flipped Queen!");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped King!");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match");
-	} else {
-		alert("Sorry, try again");
-	}
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You've found a match");
+		} else {
+			alert("Sorry, try again");
+		}
+	}	
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
