40019b8bee7b9c5cd89082f2438488dcfe383110
Michael Simmonds <michaelsimmonds92@hotmail.com>
2018-10-30 00:55:33 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen" , "queen" , "king" , "king"];
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+  },
+  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -11,13 +33,17 @@ var checkForMatch = function() {
 			alert("Sorry, try again");
 		}
 	}	
-}
+};
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
+
 
 flipCard(0);
 flipCard(2);
+
